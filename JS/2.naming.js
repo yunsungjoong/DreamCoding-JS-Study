4bbb24fa2bded3675dbbb6ff2bd27cc27eaebf8e
Y-SungJoong <yun_sj94@naver.com>
2022-10-20 16:08:41 +0900

    드림코딩 자바스크립트 공부

---
+++ JS/2.naming.js
@@ -0,0 +1,37 @@
+// 주석을 달면 됨
+
+/**
+ * 주석이됨
+ * 다음줄로 자동으로 넘어감
+ */
+
+/** 변수 규칙! 
+ * 라틴문자(0-9, a-z, A-Z,), _
+ * 대소문자를 구분함
+ * 추천 : camelCase (likeThis) 
+ * 한국어 ❌
+ * 예약어 ❌
+ * 숫자로 시작 ❌
+ * 특수문자 ❌ (_ , $ 두가지는 예외)
+ * 이모지 ❌
+*/
+
+let apple;
+let reApple;
+
+// 나쁜예제 💩
+let number = 20;
+let audio1;
+let audio2;
+
+// 좋은예제 ✨
+let myAge = 20;
+let backgroundAudio;
+let windAudio;
+
+// 꿀팁! 🍯
+let audioBackground;
+let audiowind;
+
+audioBackground;
+
